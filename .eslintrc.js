767c2e874458317b122c0d0111a433993621437a
lizhigao <lizhigao@021.com>
2020-02-28 17:59:43 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  plugins: ['xmfe'],
+  extends: ['plugin:xmfe/vue'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+};
